2f97b2d82d4c11da856401f529931260f5bb61f4
Vasavi <jellavasavi00@example.com>
2026-07-19 11:10:41 +0530

    msg

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "main" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = var.vpc_id
 
   tags = merge(
     var.sg_tags,
